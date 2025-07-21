d4eaa6b9557284852ee0156c2c5323f2846f192a
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-20 20:10:00 -0500

    set up controllers and testing table configuration

--- src/main/java/com/barrcon/patchy/controllers/UserController.java
+++ src/main/java/com/barrcon/patchy/controllers/UserController.java
@@ -1,4 +1,15 @@
 package com.barrcon.patchy.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+    @postMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+
+        return ResponseEntity.ok("User registered successfully");
+    }
 }
