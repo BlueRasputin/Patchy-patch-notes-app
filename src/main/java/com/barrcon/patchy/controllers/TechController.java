8554036c9c36fda9c0b5536e644074a64b4df0a4
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-21 15:01:10 -0500

    assign CRUD ops to TechController

--- src/main/java/com/barrcon/patchy/controllers/TechController.java
+++ src/main/java/com/barrcon/patchy/controllers/TechController.java
@@ -5,20 +5,41 @@ import com.barrcon.patchy.models.Tech;
 import com.barrcon.patchy.repositories.TechRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-//public class TechController {
-//
-//    @Autowired
-//    private TechRepository techRepository;
-//
-//
-//
-//}
+@RestController
+@RequestMapping("api/tech")
+@CrossOrigin
+public class TechController {
+
+    @Autowired
+    private TechRepository techRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Tech>> getAllTech() {
+        return ResponseEntity.ok((List<Tech>) techRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tech> getTechById(@PathVariable Long id) {
+         Optional<tech> tech = techRepository.findById(id);
+         return tech.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Tech> createTech(@RequestBody Tech tech) {
+        return ResponseEntity.ok(techRepository.save(tech));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTech(@PathVariable Long id) {
+        techRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
