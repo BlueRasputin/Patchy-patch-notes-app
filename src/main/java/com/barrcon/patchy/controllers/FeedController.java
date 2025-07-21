99c5414d60941600db634cc0d51de4baca10bce6
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-21 16:23:46 -0500

    assign CRUD ops to FavoritesController

--- src/main/java/com/barrcon/patchy/controllers/FeedController.java
+++ src/main/java/com/barrcon/patchy/controllers/FeedController.java
@@ -7,14 +7,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/feed")
-//public class FeedController {
-//
-//
-//    @Autowired
-//    private FeedRepository feedRepository;
-//    // Endpoint to get the feed for a specific user
-//    @GetMapping("/users/{id}")
-//
-//}
+
