35da4e0ae6d8e6fdfcd4674d84c6a62fbfaefa2c
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-21 19:27:33 -0500

    assigning CRUD ops for FeedController

--- src/main/java/com/barrcon/patchy/controllers/FeedController.java
+++ src/main/java/com/barrcon/patchy/controllers/FeedController.java
@@ -8,3 +8,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@RequestMapping("/api/feed")
+@CrossOrigin
+public class FeedController {
+
+    @Autowired
+    private FeedRepository feedRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Feed>> getAllFeeds() {
+        return ResponseEntity.ok((List<Feed>) feedRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Feed> getFeedById(@PathVariable Long id) {
+        Optional<Feed> feed = feedRepository.findById(id);
+        return feed.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Feed> createFeed(@RequestBody Feed feed) {
+        return ResponseEntity.ok(feedRepository.save(feed));
+    }
+
+    @PutMapping("/{id}/read")
+    public ResponseEntity<Feed> markAsRead(@PathVariable Long id) {
+        Optional<Feed> feedOptional = feedRepository.findById(id);
+        if (feedOptional.isPresent()) {
+            Feed feed = feedOptional.get();
+            feed.setRead(true);
+            return ResponseEntity.ok(feedRepository.save(feed));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFeed(@PathVariable Long id) {
+        feedRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
