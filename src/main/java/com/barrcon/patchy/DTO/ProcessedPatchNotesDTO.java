510988aa940c5b5988df76c1d98804bcf2e421ca
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-21 20:35:31 -0500

    created DTO for processed patch notes, updated user controller

---
+++ src/main/java/com/barrcon/patchy/DTO/ProcessedPatchNotesDTO.java
@@ -0,0 +1,39 @@
+package com.barrcon.patchy.DTO;
+
+public class ProcessedPatchNotesDTO {
+
+    private String version;
+    private String title;
+    private List<String> bulletPoints;
+
+    public ProcessedPatchNotesDTO() {}
+
+    public ProcessedPatchNotesDTO(String version, String title, List<String> bulletPoints) {
+        this.version = version;
+        this.title = title;
+        this.bulletPoints = bulletPoints;
+    }
+
+    // Getters and setters
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getBulletPoints() {
+        return bulletPoints;
+    }
+
+    public void setBulletPoints(List<String> bulletPoints) { this.bulletPoints = bulletPoints; }
+}
