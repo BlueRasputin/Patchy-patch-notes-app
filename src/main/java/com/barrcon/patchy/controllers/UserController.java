5f062b84a4dd594966770c2392dcc31ef782bede
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-23 18:20:28 -0500

    updating Google gemini integration

--- src/main/java/com/barrcon/patchy/controllers/UserController.java
+++ src/main/java/com/barrcon/patchy/controllers/UserController.java
@@ -1,17 +1,18 @@
 package com.barrcon.patchy.controllers;
-
+import com.barrcon.patchy.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
 
-    @postMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
-
         return ResponseEntity.ok("User registered successfully");
     }
 }
