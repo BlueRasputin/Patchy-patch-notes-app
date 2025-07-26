8744f9fdf5391381fa8f95d006691bda6b18812a
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-26 00:48:38 -0500

    removed favorites table references and files

--- src/main/java/com/barrcon/patchy/repositories/FavoritesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.barrcon.patchy.repositories;
-
-import com.barrcon.patchy.models.Favorites;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FavoritesRepository extends CrudRepository <Favorites, Long> {
-}
