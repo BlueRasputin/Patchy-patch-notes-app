7a22dc5f9e171970ab6d6d0e99846b1c9f466cb0
BlueRasputin <barrettjoshrev10@gmail.com>
2025-07-27 15:40:45 -0500

    refactor and rescope: slim down models and relationships for MVP

--- src/main/java/com/barrcon/patchy/controllers/FeedController.java
+++ src/main/java/com/barrcon/patchy/controllers/FeedController.java
@@ -1,19 +1,19 @@
-package com.barrcon.patchy.controllers;
-
-import com.barrcon.patchy.models.Feed;
-import com.barrcon.patchy.models.User;
-import com.barrcon.patchy.repositories.UserRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Optional;
-import com.barrcon.patchy.repositories.FeedRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+//package com.barrcon.patchy.controllers;
+//
+//import com.barrcon.patchy.models.Feed;
+//import com.barrcon.patchy.models.User;
+//import com.barrcon.patchy.repositories.UserRepository;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import java.util.List;
+//import java.util.Optional;
+//import com.barrcon.patchy.repositories.FeedRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
 
 //@RestController
 //@RequestMapping("/api/feed")
